7298fe9444bbbbc3a7b12737046d5873f0c24f00
Connor Manning <connor@hobu.co>
2021-08-23 11:12:19 -0500

    Remove test coverage threshold for now.

--- src/copc/copc.test.ts
+++ src/copc/copc.test.ts
@@ -3,20 +3,19 @@ import { ellipsoidFilename } from 'test'
 import { Copc } from '.'
 
 const filename = ellipsoidFilename
-// const getter = getGetter(filename)
 
 test('data', async () => {
   const copc = await Copc.create(filename)
-	const view = await Copc.loadPointData(filename, copc, '0-0-0-0')
-	const xyz = ['X', 'Y', 'Z'].map(view.getter)
+  const view = await Copc.loadPointData(filename, copc, '0-0-0-0')
+  const xyz = [view.getter('X'), view.getter('Y'), view.getter('Z')]
 
   let min = [Infinity, Infinity, Infinity]
   let max = [-Infinity, -Infinity, -Infinity]
-	for (let i = 0; i < view.pointCount; ++i) {
-		const p = xyz.map(get => get(i))
+  for (let i = 0; i < view.pointCount; ++i) {
+    const p = xyz.map((get) => get(i))
     min = p.map((v, i) => Math.min(v, min[i]))
     max = p.map((v, i) => Math.max(v, max[i]))
-	}
+  }
 
   expect(min[0]).toBeGreaterThanOrEqual(copc.header.min[0])
   expect(min[1]).toBeGreaterThanOrEqual(copc.header.min[1])
