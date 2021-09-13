0330acf61b14930731176f0b558bcc6edc8885db
Tim Whitney <tim.d.whitney@gmail.com>
2021-09-13 13:49:26 -0600

    Update create to actually work.

--- src/utils/getter.ts
+++ src/utils/getter.ts
@@ -3,11 +3,14 @@ export const Getter = { create }
 
 function create(arg: string | Getter): any {
 	if (typeof arg ==='function') return arg
-  const getter = (begin: number, end: number) => fetch(arg, {
-      headers: {
-        range: `bytes=${begin}-${end - 1}`
+  return (begin: number, end: number)  => {
+	  return fetch(arg, {
+        headers: {
+          range: `bytes=${begin}-${end - 1}`
+        }
       }
-    }
-	);
-	return getter;
+    )
+      .then(response => response.arrayBuffer())
+      .then(bufferedResponse => new Uint8Array(bufferedResponse));
+  }
 }
