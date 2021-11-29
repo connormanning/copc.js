7be9fe614dba9cb95d769f51238ee2d1ed9c2a37
Tim Whitney <tim.d.whitney@gmail.com>
2021-11-29 14:24:20 -0700

    Add retry logic for chrome issue with byte ranges.

--- src/utils/getter.ts
+++ src/utils/getter.ts
@@ -1,14 +1,27 @@
 export type Getter = (begin: number, end: number) => Promise<Uint8Array>
 export const Getter = { create }
 
+// This was taken from: https://dev.to/ycmjason/javascript-fetch-retry-upon-failure-3p6g
+const fetch_retry = (url: string, options: any, n: number): Promise<any> => 
+  fetch(url, options)
+    .catch((error) => {
+      if (n === 0) { 
+        throw error
+      };
+      return fetch_retry(url, options, n - 1);
+    });
+
 function create(arg: string | Getter): any {
 	if (typeof arg ==='function') return arg
   return (begin: number, end: number)  => {
-	  return fetch(arg, {
+	  return fetch_retry(
+      arg, 
+      {
         headers: {
           range: `bytes=${begin}-${end - 1}`
         }
-      }
+      }, 
+      1
     )
       .then(response => response.arrayBuffer())
       .then(bufferedResponse => new Uint8Array(bufferedResponse));
